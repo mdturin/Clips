da7702535398bb26f4e45e2e86f74cc17cbd1e8d
Md Turin <matkhan01@yahoo.com>
2023-03-23 03:14:29 +0600

    17 - Applying Input Masking Done

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,22 +1,29 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { InputComponent } from './input/input.component';
 import { ModalComponent } from './modal/modal.component';
-import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabComponent } from './tab/tab.component';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+import { NgxMaskModule } from 'ngx-mask';
 
 @NgModule({
   declarations: [
     ModalComponent,
     TabsContainerComponent,
-    TabComponent
+    TabComponent,
+    InputComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    NgxMaskModule.forRoot()
   ],
   exports: [
     ModalComponent,
     TabsContainerComponent,
-    TabComponent
+    TabComponent,
+    InputComponent,
   ]
 })
 export class SharedModule { }
