5962094bce7cdeb5c935b7cd734c124d99af7bf2
Md Turin <matkhan01@yahoo.com>
2023-04-07 01:55:17 +0600

    13.6 upload form done

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.css']
+  styleUrls: ['./upload.component.css'],
 })
 export class UploadComponent {
   isDragOver = false;
+  file: File | null = null;
+  nextStep: boolean = false;
 
-  storeFile($event: Event){
+  title = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3)
+  ]);
+
+  uploadForm = new FormGroup({
+    title: this.title
+  })
+
+  storeFile(event: Event) {
     this.isDragOver = false;
+
+    this.file = (event as DragEvent).dataTransfer?.files.item(0) ?? null;
+    if (!this.file || this.file.type !== 'video/mp4') {
+      return;
+    }
+
+    this.title.setValue(
+      this.file.name.replace(/\.[^/.]+$/, '')
+    )
+    this.nextStep = true;
+  }
+
+  uploadFile(){
+    console.log("File Uploaded.");
   }
 }
